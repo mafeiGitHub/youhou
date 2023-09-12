abb9936724e8958aa0e40b3ac1a51330058f6b2c
mawen <mwf1998@qq.com>
2023-09-12 22:23:03 +0800

    第三版代码提交

--- new1/jiaoben-demo.js
+++ new1/jiaoben-demo.js
@@ -1,10 +1,10 @@
 // ==UserScript==
 // @name         xiaofei
 // @namespace    http://tampermonkey.net/
-// @version      0.2
+// @version      0.3
 // @description  这是一个脚本demo
 // @author       xiaofei
-// @license      MIT
+// @license      GPL License
 // @match        *：//*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=bilibili.com
 // @grant        none
@@ -15,4 +15,5 @@
 
     console.log('nihao!!');
     console.log('我的第一个脚本！');
+    console.log('我的脚本第三版！');
 })();
