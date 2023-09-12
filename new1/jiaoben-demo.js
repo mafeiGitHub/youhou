83461e0e2e03f5607fb761d478fcd08ec3758339
mawen <mwf1998@qq.com>
2023-09-12 21:56:57 +0800

    world -> new world

--- new1/jiaoben-demo.js
+++ new1/jiaoben-demo.js
@@ -1,10 +1,11 @@
 // ==UserScript==
-// @name         New Userscript
+// @name         hello world
 // @namespace    http://tampermonkey.net/
-// @version      0.1
-// @description  try to take over the world!
-// @author       You
-// @match        https://search.bilibili.com/all?keyword=%E5%89%8D%E7%AB%AF&from_source=webtop_search&spm_id_from=444.41&search_source=5
+// @version      0.2
+// @description  这是一个脚本demo
+// @author       xiaofei
+// @license      MIT
+// @match        *：//*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=bilibili.com
 // @grant        none
 // ==/UserScript==
@@ -12,5 +13,5 @@
 (function() {
     'use strict';
 
-    console.log('Hello world!');
+    console.log('Hello  new world!');
 })();
