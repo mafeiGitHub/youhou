cc0878e1d8c527797772d8a2b05fc3f0caa636c9
mawen <mwf1998@qq.com>
2023-09-12 21:31:37 +0800

    init a hello world

---
+++ new1/jiaoben-demo.js
@@ -0,0 +1,16 @@
+// ==UserScript==
+// @name         New Userscript
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  try to take over the world!
+// @author       You
+// @match        https://search.bilibili.com/all?keyword=%E5%89%8D%E7%AB%AF&from_source=webtop_search&spm_id_from=444.41&search_source=5
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=bilibili.com
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    console.log('Hello world!');
+})();
