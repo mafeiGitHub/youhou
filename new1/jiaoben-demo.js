f1e6c9aa8e40e9215b83d3de2352e04fdc7db4f7
mawen <mwf1998@qq.com>
2023-09-13 13:27:46 +0800

    修改

--- new1/jiaoben-demo.js
+++ new1/jiaoben-demo.js
@@ -1,12 +1,13 @@
 // ==UserScript==
 // @name         xiaofei
 // @namespace    http://tampermonkey.net/
-// @version      0.8
+// @version      0.9
 // @description  这是一个脚本demo
 // @author       xiaofei
 // @license      GPL License
 // @match             *://*.bilibili.com/*
 // @match             *://*.google.com/*
+// @match             *://*.baidu.com/*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=bilibili.com
 // @grant        none
 // ==/UserScript==
@@ -21,8 +22,8 @@
     // console.log('我的脚本第5版！');
     // console.log('我的脚本第6版！');
     alert('111')
-    let ele = document.getElementsByClassName('lnXdpd');
+    let ele = document.getElementsByClassName('lg');
     console.log(ele);
-    ele.src="https://www.baidu.com/img/flexible/logo/pc/result.png"
-    ele.srcset="https://www.baidu.com/img/flexible/logo/pc/result.png"
+    ele.img.src="https://www.tmchina.net/static/image/common/logo.png"
+    
 })();
