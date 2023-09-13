e45cfb7450fbcc8a46a9591948ab1f826527887b
mawen <mwf1998@qq.com>
2023-09-13 13:42:40 +0800

    add

--- new1/jiaoben-demo.js
+++ new1/jiaoben-demo.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         xiaofei
 // @namespace    http://tampermonkey.net/
-// @version      0.9
+// @version      1.0
 // @description  这是一个脚本demo
 // @author       xiaofei
 // @license      GPL License
@@ -22,10 +22,10 @@
     // console.log('我的脚本第5版！');
     // console.log('我的脚本第6版！');
     alert('111')
-    let ele = document.getElementsByClassName('s_lg_img');
+    let ele = document.getElementsByClassName('s_ipt nobg_s_fm_focus');
     
     console.log(ele);
 
-    ele.src="https://www.tmchina.net/static/image/common/logo.png"
+    ele.value="asdasd"
     
 })();
