ac338f3bb46d754fe7d411c1462fac18c9d85832
mawen <mwf1998@qq.com>
2023-09-14 21:15:26 +0800

    升级至v1.4

--- new1/jiaoben-demo.js
+++ new1/jiaoben-demo.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         xiaofei
 // @namespace    http://tampermonkey.net/
-// @version      1.3
+// @version      1.4
 // @description  这是一个脚本demo
 // @author       xiaofei
 // @license      GPL License
@@ -14,20 +14,21 @@
   "use strict"; 
 
     let CityChangeOnce = false;
-    document.querySelector("#residedistrictbox").addEventListener("DOMNodeInserted", (e) => {
-      console.log("DOMNodeInserted 变动: ", e);
-      switch(document.querySelector('#resideprovince').value){
-        case '广东省':document.querySelector('#residecity').value = '广州市';break;
-        case '山东省':document.querySelector('#residecity').value = '济南市';break;
-        case '河南省':document.querySelector('#residecity').value = '开封市';break;
-        default:CityChangeOnce=false;
-      }
-    
-      if(CityChangeOnce===false&&document.querySelector('#residecity').value){
+    let observer = new MutationObserver(mutations=>{
+        console.log('MutationObserver:',mutations);
+        switch(document.querySelector('#resideprovince').value){
+            case '广东省':document.querySelector('#residecity').value = '广州市';break;
+            case '山东省':document.querySelector('#residecity').value = '济南市';break;
+            case '河南省':document.querySelector('#residecity').value = '开封市';break;
+            default:CityChangeOnce=false;
+          }
         
-        document.querySelector('#residecity').onchange();
-        CityChangeOnce = true;
-      }
-    });
+          if(CityChangeOnce===false&&document.querySelector('#residecity').value){
+            
+            document.querySelector('#residecity').onchange();
+            CityChangeOnce = true;
+          }
+    })
+    observer.observe(document.querySelector('#residedistrictbox'),{childList:true,addedNotes:true})
 
 })();
