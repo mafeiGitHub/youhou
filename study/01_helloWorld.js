a41dfe0f2802068edda3b285086d7365e58ba577
mawen <mwf1998@qq.com>
2023-09-15 16:28:28 +0800

    add hello world

---
+++ study/01_helloWorld.js
@@ -0,0 +1,15 @@
+// ==UserScript==
+// @name         01_hello world
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  try to take over the world!
+// @author       You
+// @match        https://bbs.tampermonkey.net.cn/
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    alert('hello world!')
+})();
